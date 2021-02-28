28e14c5fec6138f1903bc2267192c1390202c666
Houssem Dellai <houssem.dellai@live.com>
2021-02-28 21:16:26 +0000

    edited readme

---
+++ app-dotnet/Data/WebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class WebAppContext : DbContext
+    {
+        public WebAppContext (DbContextOptions<WebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp.Models.Product> Product { get; set; }
+    }
+}
--- app-dotnet/Startup.cs
+++ app-dotnet/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
 
 namespace WebApp
 {
@@ -24,6 +26,9 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<WebAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
