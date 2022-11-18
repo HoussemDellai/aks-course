93f6c343521780e87a62dc242c40085b50fe3dac
Houssem Dellai <hodellai@microsoft.com>
2022-11-18 13:12:20 +0100

    added 26, work in progress..

--- 27_https_ingress_tls_secret/commands.02.sh
+++ 27_https_ingress_tls_secret/commands.02.sh
@@ -130,49 +130,9 @@ kubectl apply -f aks-helloworld-two.yaml --namespace $NAMESPACE_APP_02
 # service/aks-helloworld-two created
 
 # configure Ingress' Public IP with DNS Name
-az network public-ip update -g MC_rg-aks-we_aks-cluster_westeurope -n kubernetes-af54fcf50c6b24d7fbb9ed6aa62bdc77 --dns-name aks-app-02
-# {
-#   "ddosSettings": {
-#     "protectionMode": "VirtualNetworkInherited"
-#   },
-#   "dnsSettings": {
-#     "domainNameLabel": "aks-app-02",
-#     "fqdn": "aks-app-02.westeurope.cloudapp.azure.com"
-#   },
-#   "etag": "W/\"1d9ccec0-6672-48ac-883b-8ad3a3b487e3\"",
-#   "id": "/subscriptions/82f6d75e-85f4-434a-ab74-5dddd9fa8910/resourceGroups/MC_rg-aks-we_aks-cluster_westeurope/providers/Microsoft.Network/publicIPAddresses/kubernetes-af54fcf50c6b24d7fbb9ed6aa62bdc77",
-#   "idleTimeoutInMinutes": 4,
-#   "ipAddress": "20.101.30.39",
-#   "ipConfiguration": {
-#     "id": "/subscriptions/82f6d75e-85f4-434a-ab74-5dddd9fa8910/resourceGroups/mc_rg-aks-we_aks-cluster_westeurope/providers/Microsoft.Network/loadBalancers/kubernetes/frontendIPConfigurations/af54fcf50c6b24d7fbb9ed6aa62bdc77",
-#     "resourceGroup": "mc_rg-aks-we_aks-cluster_westeurope"
-#   },
-#   "ipTags": [],
-#   "location": "westeurope",
-#   "name": "kubernetes-af54fcf50c6b24d7fbb9ed6aa62bdc77",
-#   "provisioningState": "Succeeded",
-#   "publicIPAddressVersion": "IPv4",
-#   "publicIPAllocationMethod": "Static",
-#   "resourceGroup": "MC_rg-aks-we_aks-cluster_westeurope",
-#   "resourceGuid": "d0b81369-691c-404e-99fd-640415a9d2a4",
-#   "sku": {
-#     "name": "Standard",
-#     "tier": "Regional"
-#   },
-#   "tags": {
-#     "k8s-azure-cluster-name": "kubernetes",
-#     "k8s-azure-service": "ingress-nginx-app-02/ingress-nginx-app-02-controller"
-#   },
-#   "type": "Microsoft.Network/publicIPAddresses",
-#   "zones": [
-#     "2",
-#     "3",
-#     "1"
-#   ]
-# }
 
-
-# Name to associate with public IP address
+###########################################################
+# Option 1: Name to associate with Azure Public IP address
 DNS_NAME="aks-app-02"
 
 # Get the resource-id of the public IP
@@ -186,16 +146,31 @@ DOMAIN_NAME_FQDN=$(az network public-ip show --ids $AZURE_PUBLIC_IP_ID --query='
 echo $DOMAIN_NAME_FQDN
 # "aks-app-02.westeurope.cloudapp.azure.com"
 
+###########################################################
+# Option 2: Name to associate with Azure DNS Zone
+DNS_NAME="aks-app-02"
+
+# Add an A record to your DNS zone
+az network dns record-set a add-record \
+    --resource-group rg-houssem-cloud-dns \
+    --zone-name "houssem.cloud" \
+    --record-set-name "*" \
+    --ipv4-address $INGRESS_PUPLIC_IP
+
+# az network public-ip update -g MC_rg-aks-we_aks-cluster_westeurope -n kubernetes-af54fcf50c6b24d7fbb9ed6aa62bdc77 --dns-name $DNS_NAME
+DOMAIN_NAME_FQDN=$DNS_NAME.houssem.cloud
+echo $DOMAIN_NAME_FQDN
+# aks-app-02.houssem.cloud
+
 # create TLS/SSL certificate
-CERT_NAME=aks-ingress-cert
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -out aks-ingress-tls.crt \
     -keyout aks-ingress-tls.key \
-    -subj "/CN=$DOMAIN_NAME_FQDN/O=aks-ingress-tls" \
+    -subj "/CN=$DOMAIN_NAME_FQDN/O=$DOMAIN_NAME_FQDN" \
     -addext "subjectAltName = DNS:$DOMAIN_NAME_FQDN"
 
 ls
-# aks-helloworld-one.yaml  aks-helloworld-two.yaml  aks-ingress-tls.crt  aks-ingress-tls.key  cert_issuer.yaml  commands.02.sh  hello-world-ingress-tls.yaml
+# aks-helloworld-one.yaml aks-helloworld-two.yaml  aks-ingress-tls.crt  aks-ingress-tls.key  cert_issuer.yaml  commands.02.sh  hello-world-ingress-tls.yaml
 
 TLS_SECRET="tls-ingress-app-02-secret"
 
@@ -292,57 +267,10 @@ kubectl get ingress --namespace $NAMESPACE_APP_02
 # hello-world-ingress          nginx-app-02   aks-app-02.westeurope.cloudapp.azure.com             80, 443   12s
 # hello-world-ingress-static   nginx-app-02   aks-app-02.westeurope.cloudapp.azure.com             80, 443   11s
 
-kubectl get pods --namespace $NAMESPACE_APP_02
-# NAME                                  READY   STATUS    RESTARTS   AGE
-# aks-helloworld-one-749789b6c5-tlgsm   1/1     Running   0          113m
-# aks-helloworld-two-5b8d45b8bf-scp8g   1/1     Running   0          113m
-
-kubectl get svc --namespace $NAMESPACE_APP_02
-# NAME                 TYPE        CLUSTER-IP   EXTERNAL-IP   PORT(S)   AGE
-# aks-helloworld-one   ClusterIP   10.0.40.82   <none>        80/TCP    113m
-# aks-helloworld-two   ClusterIP   10.0.54.42   <none>        80/TCP    113m
-
 # check app is working with HTTPS
-curl https://$INGRESS_PUPLIC_IP
+curl https://$DOMAIN_NAME_FQDN
+curl https://$DOMAIN_NAME_FQDN/hello-world-one
+curl https://$DOMAIN_NAME_FQDN/hello-world-two
 
 # check the tls/ssl certificate
 curl -v -k --resolve $DOMAIN_NAME_FQDN:443:$INGRESS_PUPLIC_IP https://$DOMAIN_NAME_FQDN
-
-
-
-
-
-# Configure HTTPS for Ingress resources
-
-# install cert-manager
-
-# Label the ingress-basic namespace to disable resource validation
-kubectl label namespace $NAMESPACE_APP_02 cert-manager.io/disable-validation=true
-# namespace/ingress-basic labeled
-# Add the Jetstack Helm repository
-helm repo add jetstack https://charts.jetstack.io
-# Update your local Helm chart repository cache
-helm repo update
-# Install the cert-manager Helm chart
-helm install cert-manager jetstack/cert-manager --namespace  $NAMESPACE_APP_02 --create-namespace --set installCRDs=true
-
-kubectl apply -f issuer.yaml -n $NAMESPACE_APP_02
-# issuer.cert-manager.io/letsencrypt created
-
-kubectl get issuer -n  $NAMESPACE_APP_02
-# NAME          READY   AGE
-# letsencrypt   True    56s
-
-kubectl apply -f hello-world-ingress-tls.yaml --namespace  $NAMESPACE_APP_02
-# ingress.networking.k8s.io/hello-world-ingress created
-# ingress.networking.k8s.io/hello-world-ingress-static created
-
-kubectl get ingress -n  $NAMESPACE_APP_02
-# NAME                         CLASS    HOSTS                                      ADDRESS   PORTS     AGE
-# cm-acme-http-solver-qccmx    <none>   aks-app-01.westeurope.cloudapp.azure.com             80        4s
-# hello-world-ingress          nginx    aks-app-01.westeurope.cloudapp.azure.com             80, 443   7s
-# hello-world-ingress-static   nginx    aks-app-01.westeurope.cloudapp.azure.com             80, 443   7s
-
-kubectl get certificate --namespace  $NAMESPACE_APP_02
-# NAME         READY   SECRET       AGE
-# tls-secret   False   tls-secret   4m10s
