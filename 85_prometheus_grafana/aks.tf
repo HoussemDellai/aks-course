73c22931d761b1d157f09e8b84bd1076bc814853
Houssem Dellai <hodellai@microsoft.com>
2023-04-01 10:26:34 +0200

    aks prom grafana

---
+++ 85_prometheus_grafana/aks.tf
@@ -0,0 +1,23 @@
+# aks cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = "westeurope"
+  resource_group_name = "rg-aks-cluster"
+  dns_prefix          = "aks"
+  kubernetes_version  = "1.25.5"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "3"
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id      = azurerm_log_analytics_workspace.workspace.id
+    msi_auth_for_monitoring_enabled = true
+  }
+}
