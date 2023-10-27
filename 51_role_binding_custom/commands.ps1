624c20e9e1cd4d019d16e175e029a0194e2baa15
Houssem Dellai <hodellai@microsoft.com>
2023-10-27 14:33:44 +0200

    added _01_vnet

---
+++ 51_role_binding_custom/commands.ps1
@@ -0,0 +1,55 @@
+# Azure Custom RBAC Role
+
+## 0. Setup demo environment
+
+# Variables
+$AKS_RG="rg-aks-cluster"
+$AKS_NAME="aks-cluster"
+
+# Create and connect to AKS cluster
+az group create --name $AKS_RG --location westeurope
+
+az aks create --name $AKS_NAME --resource-group $AKS_RG --enable-aad --enable-azure-rbac
+
+az aks get-credentials -n $AKS_NAME -g $AKS_RG --overwrite-existing
+
+# Connect to cluster
+kubelogin convert-kubeconfig -l azurecli
+
+kubectl get nodes
+# Error from server (Forbidden): nodes is forbidden: 
+# User "99b281c9-823c-4633-xxxxxx" cannot list resource "nodes" in API group "" 
+# at the cluster scope: User does not have access to the resource in Azure.
+# Update role assignment to allow access.
+
+$AKS_ID=$(az aks show -n $AKS_NAME -g $AKS_RG --query id -o tsv)
+
+$USER_ID=$(az ad signed-in-user show --query id -o tsv)
+
+az role assignment create --role "Azure Kubernetes Service RBAC Writer" --assignee $USER_ID --scope $AKS_ID/namespaces/default
+
+kubectl get pods
+# No resources found in default namespace.
+
+kubectl get pods -n kube-system
+# Error from server (Forbidden): pods is forbidden: 
+# User "99b281c9-823c-4633-af92-8ac556a19bee" cannot list resource "pods" in API group "" 
+# in the namespace "kube-system": User does not have access to the resource in Azure. 
+# Update role assignment to allow access.
+
+$SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+$SUBSCRIPTION_ID
+
+az role definition create --role-definition deployment-reader.json
+
+az role assignment create --role "AKS Deployment Reader" --assignee $USER_ID --scope $AKS_ID/namespaces/kube-system
+
+az role definition list --name "AKS Deployment Reader"
+
+kubectl get deploy -n kube-system
+
+## cleanup resources
+
+az role definition delete -n "AKS Deployment Reader"
+
+az group delete -n $AKS_RG --yes --no-wait
