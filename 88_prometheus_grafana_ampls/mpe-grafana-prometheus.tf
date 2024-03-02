55e47661706036559d81b6ecc0772e94d6e32916
Houssem Dellai <houssem.dellai@live.com>
2024-03-02 12:23:43 +0100

    88 AMPLS with MPE works!!!

--- 88_prometheus_grafana_ampls/mpe-grafana-prometheus.tf
+++ 88_prometheus_grafana_ampls/mpe-grafana-prometheus.tf
@@ -1,19 +1,5 @@
 # use AzAPI to create Grafana Managed Private Endpoint
 
-# PUT https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.Dashboard/grafana/myWorkspace/managedPrivateEndpoints/myMPEName?api-version=2023-09-01
-# {
-#   "properties": {
-#     "privateLinkResourceId": "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-000000000000/resourceGroups/xx-rg/providers/Microsoft.Kusto/Clusters/sampleKustoResource",
-#     "privateLinkResourceRegion": "West US",
-#     "groupIds": [
-#       "grafana"
-#     ],
-#     "requestMessage": "Example Request Message",
-#     "privateLinkServiceUrl": "my-self-hosted-influxdb.westus.mydomain.com"
-#   },
-#   "location": "West US"
-# }
-
 resource "azapi_resource" "mpe-grafana" {
   type      = "microsoft.dashboard/grafana/managedprivateendpoints@2023-09-01"
   name      = "mpe-grafana"
