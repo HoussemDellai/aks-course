2429021206e69eb4b4036ce27d8d33f2e9af6012
Houssem Dellai <houssem.dellai@live.com>
2024-03-09 09:47:45 +0100

    85 works

--- 85_prometheus_grafana/log_analytics-dce.tf
+++ 85_prometheus_grafana/log_analytics-dce.tf
@@ -8,9 +8,8 @@ resource "azurerm_monitor_data_collection_endpoint" "dce-log-analytics" {
 }
 
 # not required
-# associate to a Data Collection Endpoint
-# resource "azurerm_monitor_data_collection_rule_association" "dcra-dce-log-analytics-aks" {
-#   name                        = "configurationAccessEndpoint" # name is required when data_collection_rule_id is specified. And when data_collection_endpoint_id is specified, the name is populated with configurationAccessEndpoint
-#   target_resource_id          = azurerm_kubernetes_cluster.aks.id
-#   data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.dce-log-analytics.id
-# }
+resource "azurerm_monitor_data_collection_rule_association" "dcra-dce-log-analytics-aks" {
+  name                        = "configurationAccessEndpoint" # name is required when data_collection_rule_id is specified. And when data_collection_endpoint_id is specified, the name is populated with configurationAccessEndpoint
+  target_resource_id          = azurerm_kubernetes_cluster.aks.id
+  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.dce-log-analytics.id
+}
