45565c4d19f1e1cdf613dab4b59154c5b013ea3d
Houssem Dellai <houssem.dellai@live.com>
2024-04-13 21:20:03 +0200

    dns central

--- _aks_dns_central copy/rg-hub.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg-hub" {
-  name     = "rg-hub-${var.prefix}"
-  location = var.location
-}
--- _aks_dns_central copy/rg-spoke.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "rg-spoke" {
-  for_each = tomap(var.spokes)
-
-  name     = "rg-spoke-${var.prefix}-${each.key}"
-  location = var.location
-}
