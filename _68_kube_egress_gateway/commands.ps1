cfc36e6443140b6b7bf3ac49431fc2331fe10433
Houssem Dellai <hodellai@microsoft.com>
2024-11-16 08:53:48 +0100

    work in progress

---
+++ _68_kube_egress_gateway/commands.ps1
@@ -0,0 +1,43 @@
+$AKS_NAME = "aks-cluster"
+$AKS_RG = "rg-aks-cluster-egress-gw"
+$NODEPOOL_NAME = "npegress"
+$GW_PREFIX_SIZE = 30
+
+az group create --name $AKS_RG --location swedencentral
+
+az aks create -g $AKS_RG -n $AKS_NAME --enable-static-egress-gateway --network-plugin azure --network-plugin-mode overlay -k 1.30.5 --node-vm-size standard_d2pds_v6 --vm-set-type VirtualMachineScaleSets
+
+az aks nodepool add -g $AKS_RG --cluster-name $AKS_NAME --name $NODEPOOL_NAME --mode gateway --node-count 2 --gateway-prefix-size $GW_PREFIX_SIZE --node-vm-size standard_d2pds_v6
+
+az aks get-credentials -g $AKS_RG -n $AKS_NAME --overwrite-existing
+
+# create public IP prefix
+
+az network public-ip prefix create -g $AKS_RG -n pip-prefix-egress --length $GW_PREFIX_SIZE
+$IP_PREFIX_ID=$(az network public-ip prefix show -g $AKS_RG -n pip-prefix-egress --query id -o tsv)
+echo $IP_PREFIX_ID
+
+@"
+apiVersion: egressgateway.kubernetes.azure.com/v1alpha1
+kind: StaticGatewayConfiguration
+metadata:
+  name: my-static-egress-gateway
+  namespace: default
+spec:
+  gatewayNodepoolName: $NODEPOOL_NAME
+  publicIpPrefixId: $IP_PREFIX_ID
+  excludeCidrs:  # Optional
+  - 10.0.0.0/8
+  - 172.16.0.0/12
+  - 169.254.169.254/32
+"@ > static_gateway_config.yaml
+
+# grant "Network Contributor" role to AKS cluster's identity
+
+$AKS_PRINCIPAL_ID=$(az aks show -g $AKS_RG -n $AKS_NAME --query identity.principalId -o tsv)
+
+az role assignment create --role "Network Contributor" --assignee $AKS_PRINCIPAL_ID --scope $IP_PREFIX_ID
+
+kubectl apply -f static_gateway_config.yaml
+
+kubectl get staticgatewayconfigurations my-static-egress-gateway -n default -o yaml
