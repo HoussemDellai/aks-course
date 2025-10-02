9c1bd18709c7505dbebbc2f86dbf4283609a4168
Houssem Dellai <hodellai@microsoft.com>
2025-10-02 18:33:39 +0200

    work in progress

--- 750_azure_arc_kubernetes/modules/k3s/main.tf
+++ 750_azure_arc_kubernetes/modules/k3s/main.tf
@@ -131,7 +131,7 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
       "fileUris": [
           "${local.template_base_url}scripts/install-k3s-msi.sh"
       ],
-  "commandToExecute": "bash install-k3s-msi.sh ${azurerm_linux_virtual_machine.vm_linux_k3s.admin_username} ${var.client_id} ${var.client_secret} ${var.tenant_id} ${azurerm_linux_virtual_machine.vm_linux_k3s.name} ${azurerm_resource_group.rg.location} ${local.template_base_url}"    }
+  "commandToExecute": "bash install-k3s-msi.sh ${azurerm_linux_virtual_machine.vm_linux_k3s.admin_username} ${var.client_id} ${var.client_secret} ${var.tenant_id} ${azurerm_linux_virtual_machine.vm_linux_k3s.name} ${azurerm_linux_virtual_machine.vm_linux_k3s.resource_group_name} ${azurerm_resource_group.rg.location} ${local.template_base_url}"    }
 PROTECTED_SETTINGS
 
 
