8183a2be77073ad6b1405ef65109f3bda0af4810
Houssem Dellai <hodellai@microsoft.com>
2025-10-02 18:56:02 +0200

    work in progress

--- 750_azure_arc_kubernetes/modules/k3s/main.tf
+++ 750_azure_arc_kubernetes/modules/k3s/main.tf
@@ -126,13 +126,21 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
   type_handler_version       = "2.1"
   auto_upgrade_minor_version = true
 
-  protected_settings = <<PROTECTED_SETTINGS
+  settings = <<SETTINGS
     {
       "fileUris": [
           "${local.template_base_url}scripts/install-k3s-msi.sh"
       ],
   "commandToExecute": "bash install-k3s-msi.sh ${azurerm_linux_virtual_machine.vm_linux_k3s.admin_username} ${var.client_id} ${var.client_secret} ${var.tenant_id} ${azurerm_linux_virtual_machine.vm_linux_k3s.name} ${azurerm_linux_virtual_machine.vm_linux_k3s.resource_group_name} ${azurerm_resource_group.rg.location} ${local.template_base_url}"    }
-PROTECTED_SETTINGS
+SETTINGS
+
+#   protected_settings = <<PROTECTED_SETTINGS
+#     {
+#       "fileUris": [
+#           "${local.template_base_url}scripts/install-k3s-msi.sh"
+#       ],
+#   "commandToExecute": "bash install-k3s-msi.sh ${azurerm_linux_virtual_machine.vm_linux_k3s.admin_username} ${var.client_id} ${var.client_secret} ${var.tenant_id} ${azurerm_linux_virtual_machine.vm_linux_k3s.name} ${azurerm_linux_virtual_machine.vm_linux_k3s.resource_group_name} ${azurerm_resource_group.rg.location} ${local.template_base_url}"    }
+# PROTECTED_SETTINGS
 
 
   timeouts {
