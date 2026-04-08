1233d029733ba03099324c12c13aa01010ad3d6d
Houssem Dellai <houssem.dellai@live.com>
2026-04-08 16:57:02 +0200

    work in progress

--- 262_aks_agc_tf/alb_install.tf
+++ 262_aks_agc_tf/alb_install.tf
@@ -1,11 +1,12 @@
-resource "terraform_data" "install-alb-into-aks" {
+resource "terraform_data" "install_alb_into_aks" {
   triggers_replace = [
-    azurerm_kubernetes_cluster.aks.id
+    azurerm_kubernetes_cluster.aks.id,
+    azurerm_user_assigned_identity.identity_alb.client_id
   ]
 
   provisioner "local-exec" {
     command = "helm upgrade alb-controller oci://mcr.microsoft.com/application-lb/charts/alb-controller --install --namespace azure-alb-system --create-namespace --version 1.9.13 --set albController.namespace=azure-alb-system --set albController.podIdentity.clientID=${azurerm_user_assigned_identity.identity_alb.client_id}"
   }
 
-  depends_on = [ terraform_data.aks-get-credentials ]
+  depends_on = [ terraform_data.aks_get_credentials ]
 }
