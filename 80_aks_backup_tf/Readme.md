df000a0297979afd835a22bd0d1737663cad5782
Houssem Dellai <houssem.dellai@live.com>
2026-04-23 17:53:11 +0200

    work in progress

--- 80_aks_backup_tf/Readme.md
+++ 80_aks_backup_tf/Readme.md
@@ -6,6 +6,36 @@
 
 ![](images/architecture.png)
 
+### Resource Groups
+
+| Resource Group | Location | Purpose |
+|---|---|---|
+| rg-aks-1 | swedencentral | Source AKS cluster |
+| rg-aks-2 | swedencentral | Target AKS cluster (restore) |
+| rg-aks-backup | swedencentral | Backup Vault, Storage Account, Snapshots |
+
+### RBAC Role Assignments
+
+| Principal | Role | Scope |
+|---|---|---|
+| Backup Vault MSI | Reader | AKS Cluster 1 |
+| Backup Vault MSI | Reader | AKS Cluster 2 |
+| Backup Vault MSI | Reader | RG aks backup |
+| Backup Vault MSI | Disk Snapshot Contributor | RG aks backup |
+| Backup Vault MSI | Data Operator for Managed Disks | RG aks backup |
+| Backup Vault MSI | Storage Blob Data Contributor | Storage Account |
+| AKS 1 Cluster MSI | Contributor | RG aks backup |
+| AKS 2 Cluster MSI | Contributor | RG aks backup |
+| Backup Extension 1 MSI | Storage Account Contributor | Storage Account |
+| Backup Extension 2 MSI | Storage Account Contributor | Storage Account |
+
+### Trusted Access
+
+| AKS Cluster | Role | Source Resource |
+|---|---|---|
+| AKS Cluster 1 | backup-operator | Backup Vault |
+| AKS Cluster 2 | backup-operator | Backup Vault |
+
 ## Deploying the resources using Terraform
 
 To deploy the Terraform configuration files, run the following commands:
--- 80_aks_backup_tf/backup_policy.tf
+++ 80_aks_backup_tf/backup_policy.tf
@@ -1,30 +1,29 @@
 resource "azurerm_data_protection_backup_policy_kubernetes_cluster" "backup_policy_aks" {
-  name                = "backup-policy-aks"
-  resource_group_name = azurerm_data_protection_backup_vault.backup_vault.resource_group_name
-  vault_name          = azurerm_data_protection_backup_vault.backup_vault.name
-
-  backup_repeating_time_intervals = ["R/2023-05-23T02:30:00+00:00/P1W"]
+  name                            = "backup-policy-aks"
+  resource_group_name             = azurerm_data_protection_backup_vault.backup_vault.resource_group_name
+  vault_name                      = azurerm_data_protection_backup_vault.backup_vault.name
+  backup_repeating_time_intervals = ["R/2026-01-01T00:00:00+00:00/PT4H"] # every 4 hours
 
   retention_rule {
     name     = "Daily"
     priority = 25
 
     life_cycle {
-      duration        = "P84D"
+      duration        = "P3D"
       data_store_type = "OperationalStore"
     }
 
     criteria {
-      days_of_week           = ["Thursday"]
-      months_of_year         = ["November"]
-      weeks_of_month         = ["First"]
-      scheduled_backup_times = ["2023-05-23T02:30:00Z"]
+      days_of_week           = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+      months_of_year         = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+      weeks_of_month         = ["First", "Second", "Third", "Fourth", "Last"]
+      scheduled_backup_times = ["2026-05-23T02:30:00Z"]
     }
   }
 
   default_retention_rule {
     life_cycle {
-      duration        = "P14D"
+      duration        = "P3D"
       data_store_type = "OperationalStore"
     }
   }
